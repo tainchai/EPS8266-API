69a6a4e40d75b9075f34fb0846c9859bbd001c63
tainchai <42456548+tainchai@users.noreply.github.com>
2022-11-28 16:58:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
- var express = require("express");
- var app = express();
- 
- var port = process.env.PORT || 5000;
- 
- app.get("/", (req, res) => {
-     var responseObject = {
-         status: true,
-         data: { text: "hello" }
-     }
-     res.json(responseObject);
- });
- 
- app.listen(port, () => {
-    console.log("application is listening on:", port);
- });
+const http = require('http');
+const url = require('url');
+
+http
+  .createServer(function (req, res) {
+    const queryObject = url.parse(req.url, true).query;
+    console.log(queryObject);
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('HI ESP');
+  })
+   .listen(8080);
